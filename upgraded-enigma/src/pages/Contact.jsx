ac27c6d617e30d7ec92e44dbe6f9086c67f5a901
Zachary Lyda <Oth1zlyda1993@gmail.com>
2023-11-16 02:39:59 -0500

    added the validators to the contact me tab.

--- upgraded-enigma/src/pages/Contact.jsx
+++ upgraded-enigma/src/pages/Contact.jsx
@@ -1,44 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Contact() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    try {
-      const response = await fetch('/process_form', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          name: e.target.name.value,
-          email: e.target.email.value,
-          message: e.target.message.value,
-        }),
-      });
-
-      if (response.ok) {
-        console.log('Form submitted successfully!');
-      } else {
-        console.error('Form submission failed.');
-      }
-    } catch (error) {
-      console.error('Error submitting form:', error);
+    if (formData.name.trim() === '' || formData.email.trim() === '' || formData.message.trim() === '') {
+      alert('Please fill in all fields');
+      return;
     }
   };
+
   return (
     <>
       <h1>Contact Me</h1>
 
       <form id="contactForm" onSubmit={handleSubmit}>
         <label htmlFor="name">Your Name:</label>
-        <input type="text" id="name" name="name" required />
+        <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required />
 
         <label htmlFor="email">Your Email:</label>
-        <input type="email" id="email" name="email" required />
+        <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required />
 
         <label htmlFor="message">Your Message:</label>
-        <textarea id="message" name="message" rows="4" required></textarea>
+        <textarea id="message" name="message" rows="4" value={formData.message} onChange={handleChange} required></textarea>
 
         <button type="submit">Submit</button>
       </form>
